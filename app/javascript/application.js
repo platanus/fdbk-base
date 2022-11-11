f64fad3920d54429668df04da16dd8e421d434e7
Guillermo Moreno <gmq@guillermomoreno.cl>
2022-11-11 15:50:29 -0300

    feat(layout): add header component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
+import TheHeader from './components/the-header.vue';
 import './css/application.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
-    components: { App },
+    components: {
+      TheHeader,
+    },
   });
   app.mount('#vue-app');
 
---
+++ app/javascript/components/the-header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="flex h-14 w-full items-center bg-indigo-700 px-4 text-white">
+    <h1 class="font-roboto text-xl font-medium">
+      Nest
+    </h1>
+    <button class="ml-auto">
+      <svg
+        class="h-6 w-6"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"
+        />
+      </svg>
+    </button>
+  </header>
+</template>

e39666fb8d6da4abe82c34e048f0f314c72f280b
Guillermo Moreno <gmq@guillermomoreno.cl>
2022-11-14 16:09:58 -0300

    feat(devise): allow registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  respond_to :html, :json
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
